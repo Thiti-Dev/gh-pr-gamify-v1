97a60a78c272df5d74ff1d7ca64d5409b86724ee
Thiti-Dev <thiti.mwk.main@gmail.com>
2024-12-22 04:11:45 +0700

    service: implements PR's service with optional initiation pattern

---
+++ services/pr_service.go
@@ -0,0 +1,80 @@
+package services
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/Thiti-Dev/gh-pr-gamify-v1/core/fetcher"
+	"github.com/Thiti-Dev/gh-pr-gamify-v1/models"
+)
+
+type PRService struct {
+	OperatedTime time.Time
+	Ent          *models.PRItem
+}
+
+type PRServiceOption func(*PRService)
+
+func WithPRItem(ent models.PRItem) PRServiceOption {
+	return func(ps *PRService) {
+		ps.Ent = &ent
+	}
+}
+
+// Functional Options Pattern
+/*
+	prServiceWithEnt := NewPRService(time.Now(), WithPRItem(models.PRItem{}))
+	prServiceWithoutEnt := NewPRService(time.Now())
+*/
+func NewPRService(operatedTime time.Time, opts ...PRServiceOption) *PRService {
+	pr := &PRService{
+		OperatedTime: operatedTime,
+	}
+
+	for _, opt := range opts {
+		opt(pr)
+	}
+
+	return pr
+}
+
+func (s *PRService) GetYesterdayMidnightUTCBasedOnOperatedTime() time.Time {
+	return time.Date(
+		s.OperatedTime.Year(), s.OperatedTime.Month(), s.OperatedTime.Day()-1,
+		0, 0, 0, 0,
+		time.UTC,
+	)
+}
+
+func (s *PRService) GetFilteredListFromPRs(prs []models.PRItem) []models.PRItem {
+	filteredPrs := []models.PRItem{}
+	for _, pr := range prs {
+		if pr.CreatedAt.Before(s.OperatedTime) || pr.CreatedAt.Equal(s.OperatedTime) {
+			if pr.PullRequest.MergedAt != nil && pr.PullRequest.MergedAt.Before(s.GetYesterdayMidnightUTCBasedOnOperatedTime()) {
+				continue
+			}
+
+			if pr.ClosedAt != nil && pr.ClosedAt.Before(s.GetYesterdayMidnightUTCBasedOnOperatedTime()) {
+				continue
+			}
+
+			filteredPrs = append(filteredPrs, pr)
+		}
+	}
+
+	return filteredPrs
+}
+
+func (s *PRService) GetPRState() (bool, error) {
+	if s.Ent == nil {
+		return false, fmt.Errorf("failed getting pr's state as the ent has not been yet initiate")
+	}
+	return s.Ent.PullRequest.MergedAt != nil && s.Ent.PullRequest.MergedAt.Before(s.OperatedTime), nil
+}
+
+func (s *PRService) GetPRReview(f fetcher.FetcherI) ([]models.PRReview, error) {
+	if s.Ent == nil {
+		return nil, fmt.Errorf("failed getting pr's review as the ent has not been yet initiate")
+	}
+	return f.GetPullRequestReviews(s.Ent.Number)
+}
